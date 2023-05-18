2e3978d400d26682864a5b66b7c3e52101d4295f
Keethu163 <karanamkeerthanasharma@gmail.com>
2023-05-18 14:21:24 +0530

    image border changed in organizatiion

--- lib/dashboard/organization.dart
+++ lib/dashboard/organization.dart
@@ -47,107 +47,113 @@ class _OrganizationPageState extends State<OrganizationPage> {
       ),
       body: Column(
         children: [
-          Card(
-            elevation: 20,
-            child: Stack(
-              children: [
-                InkWell(
-                  onTap: () {
-                    print(currentIndex);
-                  },
-                  child: CarouselSlider(
-                    items: imageList.map((item) {
-                      return Stack(
-                        children: [
-                          Image.asset(
-                            item['image_path'],
-                            fit: BoxFit.cover,
-                            width: double.infinity,
-                          ),
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Card(
+              elevation: 20,
+              child: Stack(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      print(currentIndex);
+                    },
+                    child: CarouselSlider(
+                      items: imageList.map((item) {
+                        return Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
                               child: Image.asset(
-                                'assets/images/logo.png',
-                                width: 50,
-                                height: 50,
+                                item['image_path'],
+                                fit: BoxFit.cover,
+                                width: double.infinity,
                               ),
                             ),
-                          ),
-                          Align(
-                            alignment: Alignment.bottomRight,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Material(
-                                elevation: 10,
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.transparent,
-                                child: Container(
-                                  padding: const EdgeInsets.all(8.0),
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                      color: Colors.transparent,
-                                      width: 2.0,
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.asset(
+                                  'assets/images/logo.png',
+                                  width: 50,
+                                  height: 50,
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Material(
+                                  elevation: 10,
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.transparent,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(8.0),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.transparent,
+                                        width: 2.0,
+                                      ),
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color: Colors.white.withOpacity(0.8),
                                     ),
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    color: Colors.white.withOpacity(0.8),
-                                  ),
-                                  child: const Text(
-                                    'UNESCO',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
+                                    child: const Text(
+                                      'UNESCO',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      );
-                    }).toList(),
-                    carouselController: carouselController,
-                    options: CarouselOptions(
-                      scrollPhysics: const BouncingScrollPhysics(),
-                      autoPlay: true,
-                      aspectRatio: 2,
-                      viewportFraction: 1,
-                      onPageChanged: (index, reason) {
-                        setState(() {
-                          currentIndex = index;
-                        });
-                      },
+                          ],
+                        );
+                      }).toList(),
+                      carouselController: carouselController,
+                      options: CarouselOptions(
+                        scrollPhysics: const BouncingScrollPhysics(),
+                        autoPlay: true,
+                        aspectRatio: 2,
+                        viewportFraction: 1,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            currentIndex = index;
+                          });
+                        },
+                      ),
                     ),
                   ),
-                ),
-                Positioned(
-                  bottom: 10,
-                  left: 0,
-                  right: 0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: imageList.asMap().entries.map((entry) {
-                      return GestureDetector(
-                        onTap: () =>
-                            carouselController.animateToPage(entry.key),
-                        child: Container(
-                          width: currentIndex == entry.key ? 17 : 7,
-                          height: 7.0,
-                          margin: const EdgeInsets.symmetric(
-                            horizontal: 3.0,
+                  Positioned(
+                    bottom: 10,
+                    left: 0,
+                    right: 0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: imageList.asMap().entries.map((entry) {
+                        return GestureDetector(
+                          onTap: () =>
+                              carouselController.animateToPage(entry.key),
+                          child: Container(
+                            width: currentIndex == entry.key ? 17 : 7,
+                            height: 7.0,
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: 3.0,
+                            ),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: currentIndex == entry.key
+                                    ? Color.fromARGB(255, 2, 38, 68)
+                                    : Color.fromARGB(255, 121, 175, 237)),
                           ),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: currentIndex == entry.key
-                                  ? Color.fromARGB(255, 2, 38, 68)
-                                  : Color.fromARGB(255, 121, 175, 237)),
-                        ),
-                      );
-                    }).toList(),
+                        );
+                      }).toList(),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
